ad1f000c58da8c67a28615b94112ea3e0acbeda5
himanshushukla12 <himanshushukla.shukla3@gmail.com>
2025-08-29 11:36:14 +0530

    added 10th class section directory and moved course there

---
+++ 10th class course/Chapter3_ObjectsAndClasses/Main.java
@@ -0,0 +1,9 @@
+package Chapter3_ObjectsAndClasses;
+class Main {
+    public static void main(String[] args) {
+        char ch = 'B';
+        int code =  ch;   // convert character to ASCII code
+        System.out.println("Character: " + ch);
+        System.out.println("ASCII code: " + code);
+    }
+}
